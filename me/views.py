14a6ab0efe6bad83e723101671e73633e36b66fb
Paul Tagliamonte <tag@pault.ag>
2012-02-12 14:00:13 -0500

    small foo

--- me/views.py
+++ me/views.py
@@ -53,9 +53,37 @@ def me_edit(request):
     page = get_page( user )
 
     fields = {
+        "priv_email" : {
+            "val" : user.email,
+            "txt" : "Private Email"
+        },
         "first_name" : {
             "val" : user.first_name,
             "txt" : "First Name"
+        },
+        "last_name" : {
+            "val" : user.last_name,
+            "txt" : "Last Name"
+        },
+        "font" : {
+            "val" : page.font,
+            "txt" : "Font"
+        },
+        "title" : {
+            "val" : page.title,
+            "txt" : "Title"
+        },
+        "background" : {
+            "val" : page.background,
+            "txt" : "Background"
+        },
+        "website"   : {
+            "val" : page.website,
+            "txt" : "Website"
+        },
+        "pub_email" : {
+            "val" : page.email,
+            "txt" : "Public Email"
         }
     }
 
